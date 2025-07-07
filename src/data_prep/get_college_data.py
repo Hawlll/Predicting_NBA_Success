c5950cadd8558cdf19de1ea4bbe43541c9972712
Hawlll <144499379+Hawlll@users.noreply.github.com>
2025-07-07 15:31:41 -0400

    Create get_college_data.py

---
+++ src/data_prep/get_college_data.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+def get_college_data(college_fp, draft_fp, start_year, end_year, college_metrics_features=[]):
+
+    drafted_df = pd.read_excel(draft_fp)
+    college_df = pd.read_csv(college_fp)
+
+    #Filter by start and end year
+    drafted_df = drafted_df[(drafted_df["YEAR"] >= start_year) & (drafted_df["YEAR"] <= end_year)]
+    college_df = college_df[(college_df["year"] >= start_year) & (college_df["year"] <= end_year)]
+
+    #for duplicates player names, keep the latest year
+    college_df = college_df.sort_values(by=["year"], ascending=False).drop_duplicates(subset=["player_name"])
+
+    #Rename player_name col and select desired features
+    college_df = college_df.rename(columns={"player_name": "PLAYER"})
+    if college_metrics_features:
+
+        college_df = college_df[college_metrics_features]
+
+    merged_df = pd.merge(college_df, drafted_df, on="PLAYER", how="inner")
+
+    return merged_df.dropna().drop_duplicates(subset=["PLAYER"])
+
+
